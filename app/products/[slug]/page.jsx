e4504d64fd8cc71fc897f3d608ad27cbe89b170f
deepakraj <deepakbe2001@gmail.com>
2026-06-16 08:49:05 +0530

    fix: add missing Link import in product page

--- app/products/[slug]/page.jsx
+++ app/products/[slug]/page.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
